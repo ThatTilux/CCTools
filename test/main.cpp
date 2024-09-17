025632bcd47a334d440fd6ff62fc6588ab746da1
Ole Kuhlmann <tim.ole.kuhlmann@cern.ch>
2024-09-17 16:02:57 +0200

    Fix test main not being executed

--- test/main.cpp
+++ test/main.cpp
@@ -1,13 +1,9 @@
 #include "gtest/gtest.h"
 #include "Logger.hh"
 
-namespace CCTools {
-
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
-    Logger::deactivate();
+    CCTools::Logger::deactivate();
     return RUN_ALL_TESTS();
 }
-
-} // namespace CCTools
