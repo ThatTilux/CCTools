3ab139cbc8dc8b9a2ed0814ecd94eec2a660b5dd
ThatTilux <ThatTilux@users.noreply.github.com>
2025-02-12 10:24:46 +0000

    deploy: 99a7978bef06752d2a81acee31a80cce8ea58f72

--- classCCTools_1_1ModelCalculator.js
+++ classCCTools_1_1ModelCalculator.js
@@ -6,6 +6,7 @@ var classCCTools_1_1ModelCalculator =
     [ "calc_mesh", "classCCTools_1_1ModelCalculator.html#afa73847dd6e9170d46e2400d4ffcd4c3", null ],
     [ "get_axis_ell", "classCCTools_1_1ModelCalculator.html#a30a29f5cdfaac91cc0612c3bc439634c", null ],
     [ "get_axis_z_pos", "classCCTools_1_1ModelCalculator.html#a74c11cf4f671ad30139ee44b6053f105", null ],
+    [ "get_model_tree", "classCCTools_1_1ModelCalculator.html#a3fbb57f6069d57b225361f1efa10bdf3", null ],
     [ "has_harmonics_calc", "classCCTools_1_1ModelCalculator.html#a2cfddcd1ea8dd87f8cdfaffedfb959df", null ],
     [ "has_mesh_calc", "classCCTools_1_1ModelCalculator.html#a5eeea9111a25807c6d6364a46484baef", null ],
     [ "reload_and_calc_harmonics", "classCCTools_1_1ModelCalculator.html#a80b99a07b7187ccf6ac8d83acdc5d71b", null ],
